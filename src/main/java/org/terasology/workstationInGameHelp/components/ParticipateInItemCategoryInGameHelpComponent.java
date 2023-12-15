4650e2b8c56788aca59dfcef8d61f433863d5381
thierrywong <wong.t@sfeir.com>
2023-12-15 23:50:31 +0100

    Refactor Components without attributes to EmptyComponent

--- src/main/java/org/terasology/workstationInGameHelp/components/ParticipateInItemCategoryInGameHelpComponent.java
+++ src/main/java/org/terasology/workstationInGameHelp/components/ParticipateInItemCategoryInGameHelpComponent.java
@@ -2,15 +2,11 @@
 // SPDX-License-Identifier: Apache-2.0
 package org.terasology.workstationInGameHelp.components;
 
-import org.terasology.gestalt.entitysystem.component.Component;
+import org.terasology.gestalt.entitysystem.component.EmptyComponent;
 
 /**
  * Component for items that have help information for a workstation help system.
  * Entities that have this component are processed in {@link org.terasology.workstationInGameHelp.systems.WorkstationItemsInGameHelpCommonSystem}.
  */
-public class ParticipateInItemCategoryInGameHelpComponent implements Component<ParticipateInItemCategoryInGameHelpComponent> {
-    @Override
-    public void copyFrom(ParticipateInItemCategoryInGameHelpComponent other) {
-
-    }
+public class ParticipateInItemCategoryInGameHelpComponent extends EmptyComponent<ParticipateInItemCategoryInGameHelpComponent> {
 }
